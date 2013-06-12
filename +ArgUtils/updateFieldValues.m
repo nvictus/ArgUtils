53944bc068be61ac9b13ef14697d9af2dad3e5b6
Nezar Abdennur <nabdennur@gmail.com>
2013-06-12 14:01:54 -0400

    package import missing

--- +ArgUtils/updateFieldValues.m
+++ +ArgUtils/updateFieldValues.m
@@ -1,6 +1,6 @@
 function updated = updateFieldValues(defaults, inputs, assigned)
 % 'assigned' is a reference to a java hash set
-
+import ArgUtils.*
 updated = defaults;
 field_names = fieldnames(defaults);
 
@@ -10,7 +10,7 @@ for i = 1:length(input_names)
         name = validatestring(input_names{i}, field_names);
     catch e
         error(ArgUtils.KeyError,...
-              'Did not find a match for keyword %s.\n', input_names{i});
+              'Did not find a match for keyword %s.', input_names{i});
     end
     updated.(name) = inputs.(input_names{i});
     assigned.add(name);
